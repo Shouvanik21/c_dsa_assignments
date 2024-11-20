68bb1fc3bcc0ed4969d9325468b829d7ce2ca7a2
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-11-20 23:27:36 +0530

    assg12.write a menu driven c program to implement a linear queue using linked list

--- assg12.c
+++ assg12.c
@@ -32,3 +32,50 @@ int dequeue(){
   free(ptr);
   return x;
 }
+
+int main(){
+  int val,ch;
+  while(1){
+    printf("\nMENU:\n1.INSERT\n2.DELETE\n3.DISPLAY\n4.EXIT\n");
+    printf("enter your choice:");
+    scanf("%d",&ch);
+    switch(ch){
+      case 1:
+        printf("enter the data to be inserted:");
+        enqueue();
+        break;
+
+      case 2:
+        if(front==NULL){
+          printf("UNDERFLOW");
+        }
+        else{
+          val=dequeue();
+          printf("%d is deleted\n",val);
+        }
+        break;
+
+      case 3:
+        ptr=front;
+        if(ptr==NULL){
+          printf("Queue empty");
+        }
+        else{
+          printf("the values are:\n");
+          while(ptr!=NULL){
+            printf("%d ",ptr->data);
+            ptr=ptr->next;
+          }
+          printf("\n");
+        }
+        break;
+
+      case 4:
+        exit(0);
+
+      default:
+        printf("wrong choice input");    
+    }
+  }
+  return 0;
+}
