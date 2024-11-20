beba770c067b4164d5014ad8af43db29aa2c2f6d
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-11-20 23:59:03 +0530

    updated commit

--- assg10.c
+++ assg10.c
@@ -7,3 +7,46 @@ struct node{
     struct node *next;
 };
 struct node *ptr,*stack,*top=NULL;
+
+int val,k;
+void push(char val){
+    ptr=(struct node*)malloc(sizeof(struct node));
+    ptr->data=val;
+    ptr->next=top;
+    top=ptr;
+}
+
+int pop(){
+    ptr=top;
+    val=ptr->data;
+    top=ptr->next;
+    free(ptr);
+    return val;
+}
+
+int precedence(char x){
+    switch(x){
+        case '^':
+            return (3);
+            break;
+
+        case '/':
+            return (2);
+            break;
+
+        case '*':
+            return (2);
+            break;
+
+        case '+':
+            return (1);
+            break;
+
+        case '-':
+            return (1);
+            break;
+
+        default:
+            return 0;  
+    }
+}
