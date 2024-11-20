52d7b911b887bee86d7a5c40cffdb328e661c293
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-11-20 23:26:09 +0530

    updated commit

--- assg12.c
+++ assg12.c
@@ -6,3 +6,29 @@ typedef struct node{
   struct node *next;
 }nn;
 nn *ptr,*start='\0',*front=NULL,*rear=NULL;
+
+void enqueue(){
+  ptr=(nn*)malloc(sizeof(nn));  
+  scanf("%d",&ptr->data);  
+  ptr->next=NULL;
+  if(front==NULL && rear==NULL){
+    front=ptr;
+    rear=ptr;
+  }
+  else{
+    rear->next=ptr;
+    rear=ptr;
+  }
+}
+
+int dequeue(){
+  int x;
+  x=front->data;
+  ptr=front;
+  front=front->next;
+  if(front==NULL){
+    rear=NULL;
+  }
+  free(ptr);
+  return x;
+}
