c3e51f0a0c583692d1ae10c3a1d831627c1af8fd
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-11-20 23:25:46 +0530

    updated commit

--- assg12.c
+++ assg12.c
@@ -4,17 +4,5 @@
 typedef struct node{
   int data;
   struct node *next;
-}str;
-str *ptr,*start='\0',*front=NULL,*rear=NULL;
-
-void enqueue(int x){
-  node=(nn*)malloc(sizeof(nn));
-}
-
-int dequeue(){
-  int x;
-}
-
-int main(){
-  return 0;
-}
+}nn;
+nn *ptr,*start='\0',*front=NULL,*rear=NULL;
