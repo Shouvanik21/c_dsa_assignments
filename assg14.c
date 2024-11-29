6ebf1de8b70224b2814b40dd3d7d7136a76bd6eb
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-11-29 22:15:12 +0530

    updated commit

--- assg14.c
+++ assg14.c
@@ -1,2 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+struct node{
+	int data;
+	struct node *left,*right;
+};
+
+struct node* newnode(int a)
+{
+	struct node* leaf=(struct node*)malloc(sizeof(struct node));
+	leaf->data=a;
+	leaf->left=leaf->right=NULL;
+	return leaf;
+}
+
+struct node* insert(struct node* ptr,int key)
+{
+	if(ptr==NULL)
+		return newnode(key);
+	if(key<ptr->data)
+		ptr->left=insert(ptr->left,key);
+	else
+		ptr->right=insert(ptr->right,key);
+	return ptr;	
+}
